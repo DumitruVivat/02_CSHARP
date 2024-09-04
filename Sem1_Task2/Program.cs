8678e2d999ee608441c55e4e832bc9909f401890
Dumitru Vivat <kaibaman94@gmail.com>
2024-09-04 15:55:50 +0300

    add Task2

---
+++ Sem1_Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using Internal;
+
+Console.WriteLine("Input hundred digid: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99 && num < 1000)
+{
+  int firstDigit = num / 100; //456 /10=4.56
+  int lastDigit = num % 10; // 456 % 10 = 450+6
+
+  int result = lastDigit + firstDigit;
+  Console.WriteLine($"sum of first and last digit = {result}");
+}
+else
+{
+  Console.WriteLine("wrong input!");
+}
