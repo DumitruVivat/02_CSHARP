9488281530fcd52e887ebc93239247f949edb919
Dumitru Vivat <kaibaman94@gmail.com>
2024-09-04 14:11:34 +0300

    add squer function

--- Sem1_Task0/Program.cs
+++ Sem1_Task0/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("");
+﻿// string num1str = Console.ReadLine();
+// int num = Convert.ToInt32(num1str);
+
+Console.WriteLine("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2 * num2)
+{
+  Console.WriteLine("First number is square of second number");
+}
+else
+{
+  Console.WriteLine("First numer is not squer of second number");
+}
