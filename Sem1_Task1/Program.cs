f1a3a145f4d58941f5aac792febae632b0f02a01
Dumitru Vivat <kaibaman94@gmail.com>
2024-09-04 14:48:49 +0300

    add Task1

---
+++ Sem1_Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Input even number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 0)
+{
+
+  int count = -num; // num * -1
+
+  while (count <= num)
+  {
+    Console.Write(count + " ");
+    count++;
+  }
+}
+else
+{
+  Console.WriteLine("Wrong input number!");
+}
